2cd1de05c244a618af96aade030dcdb8fe84f928
Pepi74 <pepigaming74@gmail.com>
2025-06-18 21:33:02 -0400

    Implementar WebSocket

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express, { json } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 import indexRoutes from './routes/index.js'
 import camarasRoutes from './routes/camaras.js'
 import alertasRoutes from './routes/alertas.js'
@@ -8,6 +10,15 @@ import alertasRoutes from './routes/alertas.js'
 dotenv.config()
 
 const app = express()
+const httpServer = createServer(app); // Servidor HTTP base
+const io = new Server(httpServer, {
+  cors: {
+    origin: `${process.env.FRONTEND_URL}`,
+  },
+});
+
+// Exportamos `io` para usarlo en otras partes
+export { io };
 
 app.use(cors())
 app.use(json())
@@ -16,7 +27,16 @@ app.use('/', indexRoutes)
 app.use('/api/camaras', camarasRoutes)
 app.use('/api/alertas', alertasRoutes)
 
+// WebSocket: manejar conexiones entrantes
+io.on('connection', (socket) => {
+  //console.log('Cliente conectado vía WebSocket');
+
+  socket.on('disconnect', () => {
+    //console.log('Cliente desconectado');
+  });
+});
+
 const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en puerto ${PORT}`)
-})
+httpServer.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+});
