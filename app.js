3bd0b630263dd546c572b104f06b183a11b9a045
claudio_x10 <claudio.vergarara@sansano.usm.cl>
2025-06-18 20:38:45 -0400

    Video streaming
    1.- Se agrega la carpeta python-streaming que genera el endpoint para ver la camara

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import indexRoutes from './routes/index.js'
 import camarasRoutes from './routes/camaras.js'
 import alertasRoutes from './routes/alertas.js'
+import tranmisionRoutes from './routes/transmision.js'
 
 dotenv.config()
 
@@ -15,6 +16,7 @@ app.use(json())
 app.use('/', indexRoutes)
 app.use('/api/camaras', camarasRoutes)
 app.use('/api/alertas', alertasRoutes)
+app.use('/api/transmision', tranmisionRoutes)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
