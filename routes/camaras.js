c83e96a70254b48c89f6d72a88581e7e7b2dc1c4
Pepi74 <pepigaming74@gmail.com>
2025-06-18 23:50:51 -0400

    Implementado endpoint cantidad-alertas en camaras

--- routes/camaras.js
+++ routes/camaras.js
@@ -27,4 +27,37 @@ router.get('/', async (_, res) => {
   }
 })
 
+/* Para este endpoint se asume que se tiene creado una vista en la base de datos de la siguiente forma:
+CREATE OR REPLACE VIEW camaras_con_alertas AS
+SELECT
+    c.id,
+    c.nombre,
+    c.posicion,
+    c.direccion,
+    c.estado_camara,
+    c.ultima_conexion,
+    c.link_camara,
+    COUNT(a.id) AS total_alertas
+FROM
+    camaras c
+LEFT JOIN
+    alertas a ON c.id = a.id_camara
+GROUP BY
+    c.id, c.nombre, c.posicion, c.direccion, c.estado_camara, c.ultima_conexion, c.link_camara;
+*/
+router.get('/cantidad-alertas', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM camaras_con_alertas');
+    // Convierte los BigInt de string a enteros
+    const datosConvertidos = result.rows.map(cam => ({
+      ...cam,
+      total_alertas: Number(cam.total_alertas),
+    }));
+    res.json(datosConvertidos);
+  } catch (err) {
+    console.error('Error al obtener cámaras con alertas:', err);
+    res.status(500).json({ error: 'Error al obtener cámaras con alertas' });
+  }
+})
+
 export default router
