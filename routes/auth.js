16092d1649a5dd4f8e9e628defe83666541f9c82
Pepi74 <pepigaming74@gmail.com>
2025-10-15 20:19:54 -0300

    cambio en endpoint logout con manejo limpieza cookie

--- routes/auth.js
+++ routes/auth.js
@@ -51,7 +51,11 @@ router.post('/login', async (req, res) => {
 })
 
 router.post("/logout", (_, res) => {
-  res.clearCookie("token")
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+  })
   res.json({ mensaje: "Sesión cerrada" })
 })
 
